ff2ef2554627d429ecd839b31cb010a881c4fb8b
Pasquale Lombardi <plombardi85@gmail.com>
2026-02-28 03:36:05 +0100

    Refine contact box style and restore modern Facebook iframe

--- modules/mod_itpfblikebox/tmpl/default.php
+++ modules/mod_itpfblikebox/tmpl/default.php
@@ -24,91 +24,31 @@ defined( "_JEXEC" ) or die;?>
 <?php return; ?>
 <?php endif; ?>
 
+<?php
+$fbWidth = (int) $params->get("fbWidth", 340);
+$fbHeight = (int) $params->get("fbHeight", 480);
+$fbShowFaces = $params->get("fbFaces", 1) ? 'true' : 'false';
+$fbShowTimeline = $params->get("fbStream", 1) ? 'timeline' : '';
+$fbHideCover = $params->get("fbHeader", 1) ? 'false' : 'true';
+$facebookPageHref = str_replace('http://', 'https://', $facebookPageLink);
+$facebookIframeSrc = 'https://www.facebook.com/plugins/page.php?href=' . rawurlencode($facebookPageHref)
+    . '&tabs=' . rawurlencode($fbShowTimeline)
+    . '&width=' . $fbWidth
+    . '&height=' . $fbHeight
+    . '&small_header=false&adapt_container_width=true&hide_cover=' . $fbHideCover
+    . '&show_facepile=' . $fbShowFaces;
+?>
+
 <?php if (!empty($facebookPageLink)): ?>
-<a href="<?php echo htmlspecialchars($facebookPageLink, ENT_COMPAT, 'UTF-8');?>" rel="noopener noreferrer" target="_blank">Visita la nostra pagina Facebook</a>
+<iframe
+src="<?php echo htmlspecialchars($facebookIframeSrc, ENT_COMPAT, 'UTF-8');?>"
+scrolling="no"
+frameborder="0"
+style="border:none; overflow:hidden; width:100%; max-width:<?php echo $fbWidth;?>px; height:<?php echo $fbHeight;?>px;"
+allow="encrypted-media; web-share"
+allowfullscreen="true"></iframe>
 <?php endif; ?>
-</div>
-<?php return; ?>
 
-<?php switch ($params->get("fbRendering",0)){ 
-    
-    case 1: // XFBML ?>
-
-<?php if($params->get("fbLoadJsLib", 1)) {?>
-<?php if($params->get("facebookRootDiv", 1)) {?>
-<div id="fb-root"></div>
-<?php }?>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/<?php echo $locale;?>/all.js#xfbml=1<?php echo $facebookLikeAppId;?>";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<?php }?>
-    
-    <fb:like-box 
-    href="<?php echo $params->get("fbPageLink");?>" 
-    width="<?php echo $params->get("fbWidth");?>"
-    height="<?php echo $params->get("fbHeight");?>"
-    colorscheme="<?php echo $params->get("fbColour");?>" 
-    show_faces="<?php echo (!$params->get("fbFaces")) ? "false" : "true";?>" 
-    border_color="<?php echo $params->get("fbBColour", "");?>" 
-    stream="<?php echo (!$params->get("fbStream")) ? "false" : "true";?>" 
-    header="<?php echo (!$params->get("fbHeader")) ? "false" : "true";?>"
-    force_wall="<?php echo (!$params->get("facebookForceWall")) ? "false" : "true";?>"></fb:like-box>
-
-<?php break; ?>
-
-
-<?php case 2: // HTML5 ?>
-
-<?php if($params->get("fbLoadJsLib", 1)) {?>
-
-<?php if($params->get("facebookRootDiv", 1)) {?>
-<div id="fb-root"></div>
-<?php }?>
-
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/<?php echo $locale;?>/all.js#xfbml=1<?php echo $facebookLikeAppId;?>";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<?php }?>
-
-	<div class="fb-like-box" 
-	data-href="<?php echo $params->get("fbPageLink");?>" 
-	data-width="<?php echo $params->get("fbWidth");?>" 
-	data-height="<?php echo $params->get("fbHeight");?>" 
-	data-show-faces="<?php echo $params->get("fbFaces", 1); ?>" 
-	data-border-color="<?php echo $params->get("fbBColour", "");?>" 
-	data-stream="<?php echo $params->get("fbStream", 1); ?>" 
-	data-header="<?php echo $params->get("fbHeader", 1); ?>"
-	data-force-wall="<?php echo (!$params->get("facebookForceWall")) ? "false" : "true";?>"></div>
-<?php break; ?>
-
-
-<?php default: // iframe ?>
-
-<?php 
-	$forceWall ="";
-	if($params->get("facebookForceWall"))  {
-		$forceWall = '&amp;force_wall=true';
-	}
- ?>
-
-<iframe 
-src="https://www.facebook.com/plugins/likebox.php?href=<?php echo $params->get("fbPageLink");?>&amp;locale=<?php echo $locale;?>&amp;width=<?php echo $params->get("fbWidth");?>&amp;colorscheme=<?php echo $params->get("fbColour");?>&amp;show_faces=<?php echo $params->get("fbFaces", 1);?>&amp;border_color=<?php echo rawurlencode($params->get("fbBColour", ""));?>&amp;stream=<?php echo $params->get("fbStream", 1);?>&amp;header=<?php echo $params->get("fbHeader", 1);?>&amp;height=<?php echo $params->get("fbHeight");?><?php echo $facebookLikeAppId;?><?php echo $forceWall;?>"
-scrolling="no" 
-frameborder="0" 
-style="border:none; overflow:hidden; width:<?php echo $params->get("fbWidth");?>px; height:<?php echo $params->get("fbHeight");?>px;" 
-allowTransparency="true"></iframe>
-
-<?php break; ?>
-
-<?php }// END switch?>
 <?php if (!empty($facebookPageLink)): ?>
 <div style="margin-top:8px;">
   <a href="<?php echo htmlspecialchars($facebookPageLink, ENT_COMPAT, 'UTF-8');?>" rel="noopener noreferrer" target="_blank">Visita la nostra pagina Facebook</a>
